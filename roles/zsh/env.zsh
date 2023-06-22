25394fde63f7e6fe151b936c90c65ecfec0f5772
Sly Muzzle <slymuzzle@gmail.com>
2023-06-22 17:48:52 +0600

    Fixes

--- roles/zsh/env.zsh
+++ roles/zsh/env.zsh
@@ -2,3 +2,7 @@
 DISABLE_CORRECTION="true"
 unsetopt correct
 unsetopt correct_all
+
+export ZK_NOTEBOOK_DIR="$HOME/Wiki/sly-wiki.md"
+
+export OPENAI_API_KEY=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
